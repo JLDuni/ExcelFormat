ef00fc591a1f6e9338a947be3803229b07d75d72
JLDuni <uc2023214314@student.uc.pt>
2026-09-25 18:34:13 +0100

    removed extra text

--- src/app.py
+++ src/app.py
@@ -3,9 +3,6 @@ import pandas as pd
 from format import process_contracts
 
 st.title("Excel Formatter")
-st.write(
-    "Upload your contracts XLSX file to format the license plates and export to CSV."
-)
 
 uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx"])
 
@@ -14,7 +11,7 @@ if uploaded_file is not None:
 
     csv_data = process_contracts(raw_df)
 
-    output_filename = st.text_input("Name your output file:", value="formatted_contracts.csv")
+    output_filename = st.text_input("Name your file:", value="formatted_contracts.csv")
 
     if not output_filename.endswith(".csv"):
         output_filename += ".csv"
